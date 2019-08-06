58c351ce4f36d7f2dc090f9c5086c3352b9e2a0d
Mattia Bonazzi <40828778+mattia0690@users.noreply.github.com>
2019-08-06 18:31:21 +0100

    Add files via upload

---
+++ projects/Capstone_Project-Downlink_data_traffic_prediction_for_new_4G_cell_deployments/visuals.py
@@ -0,0 +1,285 @@
+###########################################
+# Suppress matplotlib user warnings
+# Necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+#
+# Display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+###########################################
+
+import matplotlib.pyplot as pl
+import matplotlib.patches as mpatches
+import numpy as np
+import pandas as pd
+from time import time
+import math
+import sklearn.learning_curve as curves
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import ShuffleSplit, train_test_split
+from sklearn.grid_search import GridSearchCV
+import matplotlib.pyplot as pl
+import matplotlib.patches as mpatches
+
+def distribution(data, transformed = False):
+    """
+    Visualization code for displaying skewed distributions of features
+    """
+    
+    # Create figure
+    fig = pl.figure(figsize = (11,5));
+
+    # Skewed feature plotting
+    for i, feature in enumerate(['day_30','day_29']):
+		#i, feature in enumerate(['day_30','day_29','day_28','day_27','day_26','day_25','day_24','day_23','day_22','day_21','day_20','day_19','day_18','day_17','day_16','day_15','day_14','day_13','day_12','day_11','day_10','day_9','day_8','day_7','day_6','day_5','day_4','day_3','day_2','day_1']):
+        ax = fig.add_subplot(1, 2, i+1)
+        ax.hist(data[feature], bins = 25, color = '#00A0A0')
+        ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 14)
+        ax.set_xlabel("Value")
+        ax.set_ylabel("Number of Records")
+        #ax.set_ylim((0, 2000))
+        #ax.set_yticks([0, 500, 1000, 1500, 2000])
+        #ax.set_yticklabels([0, 500, 1000, 1500, ">2000"])
+        if transformed:
+            ax.set_xticks(np.arange(0, math.ceil(max(data[feature]))+1, (math.ceil(max(data[feature]))+1)/5))
+        else:
+            ax.set_xticks(np.arange(0, (math.ceil(max(data[feature])/10000))*10000, (math.ceil(max(data[feature])/10000))*10000/5))
+
+    # Plot aesthetics
+    if transformed:
+        fig.suptitle("Log-transformed Distributions of Continuous Data", \
+            fontsize = 16, y = 1.03)
+    else:
+        fig.suptitle("Skewed Distributions of Continuous Data", \
+            fontsize = 16, y = 1.03)
+
+    fig.tight_layout()
+    fig.show()
+
+
+def ModelLearning(X, y):
+    """ Calculates the performance of several models with varying sizes of training data.
+        The learning and testing scores for each model are then plotted. """
+    
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+
+    # Generate the training set sizes increasing by 50
+    train_sizes = np.rint(np.geomspace(10, X.shape[0]*0.8 - 10, 15)).astype(int)
+
+    # Create the figure window
+    fig = pl.figure(figsize=(10,7))
+
+    # Create three different models based on max_depth
+    for k, depth in enumerate([1,3,6,10]):
+        
+        # Create a Decision tree regressor at max_depth = depth
+        regressor = RandomForestRegressor(max_depth = depth)
+
+        # Calculate the training and testing scores
+        sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
+            cv = cv, train_sizes = train_sizes, scoring = 'r2')
+        
+        # Find the mean and standard deviation for smoothing
+        train_std = np.std(train_scores, axis = 1)
+        train_mean = np.mean(train_scores, axis = 1)
+        test_std = np.std(test_scores, axis = 1)
+        test_mean = np.mean(test_scores, axis = 1)
+
+        # Subplot the learning curve 
+        ax = fig.add_subplot(2, 2, k+1)
+        ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
+        ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
+        ax.fill_between(sizes, train_mean - train_std, \
+            train_mean + train_std, alpha = 0.15, color = 'r')
+        ax.fill_between(sizes, test_mean - test_std, \
+            test_mean + test_std, alpha = 0.15, color = 'g')
+        
+        # Labels
+        ax.set_title('max_depth = %s'%(depth))
+        ax.set_xlabel('Number of Training Points')
+        ax.set_ylabel('Score')
+        ax.set_xlim([0, X.shape[0]*0.8])
+        ax.set_ylim([-0.05, 1.05])
+    
+    # Visual aesthetics
+    ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
+    fig.suptitle('Random Forest Regressor Learning Performances', fontsize = 16, y = 1.03)
+    fig.tight_layout()
+    fig.show()
+
+
+def ModelComplexity(X, y):
+    """ Calculates the performance of the model as model complexity increases.
+        The learning and testing errors rates are then plotted. """
+    
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+
+    # Vary the max_depth parameter from 1 to 10
+    max_depth = np.arange(1,11)
+
+    # Calculate the training and testing scores
+    train_scores, test_scores = curves.validation_curve(RandomForestRegressor(), X, y, \
+        param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
+
+    # Find the mean and standard deviation for smoothing
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    # Plot the validation curve
+    pl.figure(figsize=(7, 5))
+    pl.title('Random Forest Regressor Complexity Performance')
+    pl.plot(max_depth, train_mean, 'o-', color = 'r', label = 'Training Score')
+    pl.plot(max_depth, test_mean, 'o-', color = 'g', label = 'Validation Score')
+    pl.fill_between(max_depth, train_mean - train_std, \
+        train_mean + train_std, alpha = 0.15, color = 'r')
+    pl.fill_between(max_depth, test_mean - test_std, \
+        test_mean + test_std, alpha = 0.15, color = 'g')
+    
+    # Visual aesthetics
+    pl.legend(loc = 'lower right')
+    pl.xlabel('Maximum Depth')
+    pl.ylabel('Score')
+    pl.ylim([-0.05,1.05])
+    pl.show()
+
+
+def feature_plot(importances, X_train, y_train):
+    
+    # Display the five most important features
+    indices = np.argsort(importances)[::-1]
+    columns = X_train.columns.values[indices[:5]]
+    values = importances[indices][:5]
+
+    # Creat the plot
+    fig = pl.figure(figsize = (9,5))
+    pl.title("Normalized Weights for First Five Most Predictive Features", fontsize = 16)
+    pl.bar(np.arange(5), values, width = 0.6, align="center", color = '#00A000', \
+          label = "Feature Weight")
+    pl.bar(np.arange(5) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
+          label = "Cumulative Feature Weight")
+    pl.xticks(np.arange(5), columns)
+    pl.xlim((-0.5, 4.5))
+    pl.ylabel("Weight", fontsize = 12)
+    pl.xlabel("Feature", fontsize = 12)
+    
+    pl.legend(loc = 'upper center')
+    pl.tight_layout()
+    pl.show()  
+    
+        
+def PredictTrials(X, y, data):
+
+    """ Performs trials of fitting and predicting data. """
+
+    # Store the predicted prices
+    prices = []
+
+    for k in range(10):
+        # Split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, \
+            test_size = 0.2, random_state = k)
+        
+        # Fit the data
+        #reg = fitter(X_train, y_train)
+        reg = RandomForestRegressor(random_state=2)
+
+        # Create the parameters list that will be tuned
+        parameters = {
+        'n_estimators' : [150,175],
+        'max_depth' : [10,11]
+        }
+              
+        # Perform grid search on the regressor 
+        grid_obj = GridSearchCV(reg, parameters)
+
+        # Fit the grid search object to the training data and find the optimal parameters using fit()
+        grid_fit = grid_obj.fit(X_train, np.ravel(y_train))
+
+        # Get the best estimator
+        best_reg = grid_fit.best_estimator_
+        
+        # Make a prediction
+        pred = best_reg.predict(data)
+        prices.append(pred)
+        
+        # Result
+        print("Trial {}: Mbit {:,.0f}".format(k+1, pred[0]))
+
+    # Display price range
+    print("\nRange in Data Volume: Mbit {}".format(str(max(prices) - min(prices))))
+    
+    
+def evaluate(results, accuracy, f1):
+    """
+    Visualization code to display results of various learners.
+    
+    inputs:
+      - learners: a list of supervised learners
+      - stats: a list of dictionaries of the statistic results from 'train_predict()'
+      - accuracy: The score for the naive predictor
+      - f1: The score for the naive predictor
+    """
+  
+    # Create figure
+    fig, ax = pl.subplots(2, 3, figsize = (11,7))
+
+    # Constants
+    bar_width = 0.3
+    colors = ['#A00000','#00A0A0','#00A000']
+    
+    # Super loop to plot four panels of data
+    for k, learner in enumerate(results.keys()):
+        for j, metric in enumerate(['train_time', 'R2_train', 'MSE_train', 'pred_time', 'R2_test', 'MSE_test']):
+            for i in np.arange(3):
+                
+                # Creative plot code
+                ax[j//3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
+                ax[j//3, j%3].set_xticks([0.45, 1.45, 2.45])
+                ax[j//3, j%3].set_xticklabels(["10%", "30%", "100%"])
+                #ax[j//3, j%3].set_xlabel("Training Set Size")
+                ax[j//3, j%3].set_xlim((-0.1, 3.0))
+    
+    # Add unique y-labels
+    ax[0, 0].set_ylabel("Time (in seconds)")
+    #ax[0, 1].set_ylabel("R2 Score")
+    #ax[0, 2].set_ylabel("MSE score")
+    ax[1, 0].set_ylabel("Time (in seconds)")
+    #ax[1, 1].set_ylabel("R2 Score")
+    #ax[1, 2].set_ylabel("MSE score")
+    
+    # Add titles
+    ax[0, 0].set_title("Model Training")
+    ax[0, 1].set_title("R2 Score on Training Subset")
+    ax[0, 2].set_title("MSE on Training Subset scaled by 10e10")
+    ax[1, 0].set_title("Model Predicting")
+    ax[1, 1].set_title("R2 Score on Testing")
+    ax[1, 2].set_title("MSE on Testing Set Set scaled by 10e10")
+    
+    # Add horizontal lines for naive predictors
+    ax[0, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 1].axhline(y = accuracy, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[0, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    ax[1, 2].axhline(y = f1, xmin = -0.1, xmax = 3.0, linewidth = 1, color = 'k', linestyle = 'dashed')
+    
+    # Set y-limits for score panels
+    ax[0, 1].set_ylim((-0.25, 1))
+    ax[0, 2].set_ylim((0, 1))
+    ax[1, 1].set_ylim((-0.25, 1))
+    ax[1, 2].set_ylim((0, 1))
+
+    # Create patches for the legend
+    patches = []
+    for i, learner in enumerate(results.keys()):
+        patches.append(mpatches.Patch(color = colors[i], label = learner))
+    pl.legend(handles = patches, bbox_to_anchor = (-.80, 2.53), \
+               loc = 'upper center', borderaxespad = 0., ncol = 3, fontsize = 'x-large')
+    
+    # Aesthetics
+    pl.suptitle("Performance Metrics for Three Supervised Learning Models", fontsize = 16, y = 1.10)
+    #pl.tight_layout()
+    pl.show()
